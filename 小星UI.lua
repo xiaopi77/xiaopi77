3f5c004a91eced32e0e31085b5908b46f8fe3815
xiaopi77 <2131869117@qq.com>
2024-08-25 17:56:22 +0800

    Add files via upload

--- 小星UI.lua
+++ 小星UI.lua
@@ -211,7 +211,7 @@ end
       DropShadow.BackgroundTransparency = 1.000
       DropShadow.Position = UDim2.new(0.5, 0, 0.5, 0)
       DropShadow.Size = UDim2.new(1, 10, 1, 10)
-      DropShadow.Image = "16060333448"
+      DropShadow.Image = "rbxassetid://16060333448"
       DropShadow.ImageColor3 = Color3.fromRGB(255,255,255)
       DropShadow.SliceCenter = Rect.new(49, 49, 450, 450)
 
