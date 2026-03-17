0c26870d3b2966dde279fe7e8583763df14360a1
xiaopi77 <2131869117@qq.com>
2026-03-17 19:50:47 +0800

    Update Roblox-Pi-Script-administrator.lua

--- Roblox-Pi-Script-administrator.lua
+++ Roblox-Pi-Script-administrator.lua
@@ -1,9 +1,10 @@
 return {
 --用户ID
     123456789,     
-    987654321, 
+    987654321,
+    10521762428
 --用户名        
     "shdhudgwu",  
     "xiaopi91",   
-    "ServerOwner"
+    "xiaopi333"
 }
