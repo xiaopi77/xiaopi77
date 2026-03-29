82d2882bc3ab7aee930773d9b27fecf82239a1de
xiaopi77 <2131869117@qq.com>
2026-03-29 09:58:57 +0800

    Add files via upload

---
+++ Roblox-Pi-Script-VIPUsers.lua
@@ -0,0 +1,25 @@
+return {
+    ["xiaopi333"] = {
+        title = "皮脚本作者",
+        chatMessage = "👑欢迎皮脚本作者: [player] 加入服务器！",
+        notification = "皮脚本作者: [player] 加入服务器"
+    },
+    
+    ["xiaopi91"] = {
+        title = "皮脚本作者",
+        chatMessage = "👑欢迎皮脚本作者: [player] 加入服务器！",
+        notification = "皮脚本作者 [player] 加入服务器"
+    },
+    
+    ["chinawerui"] = {
+        title = "叶脚本作者",
+        chatMessage = "👑欢迎叶脚本作者: [player] 加入服务器！",
+        notification = "叶脚本作者 [player] 加入服务器"
+    },
+    
+    ["dsafaac"] = {
+        title = "长夜月",
+        chatMessage = "欢迎皮脚本VIP用户进入服务器",
+        notification = "皮脚本VIP用户 [player] 加入服务器"
+    },    
+}
