6b617465f5358cae099c907cdabb1e5293552916
xiaopi77 <2131869117@qq.com>
2026-03-29 10:50:32 +0800

    Update Roblox-Pi-Script-VIPUsers.lua

--- Roblox-Pi-Script-VIPUsers.lua
+++ Roblox-Pi-Script-VIPUsers.lua
@@ -2,21 +2,26 @@ return {
     ["xiaopi333"] = {
         title = "皮脚本作者",
         welcomeMsg = "👑欢迎皮脚本作者: [player] 加入服务器！",
-        notification = "皮脚本作者: [player] 加入服务器"
+        notification = "皮脚本作者: [player] 加入服务器",
+        -- 权限设置（可选，默认为true）
+        showOtherTitle = true,        -- 是否显示其他VIP用户的头衔
+        receiveWelcomeMsg = true,     -- 是否接收其他VIP的欢迎消息
+        receiveNotification = true    -- 是否接收其他VIP的通知
     },
     ["xiaopi91"] = {
         title = "皮脚本作者",
         welcomeMsg = "👑欢迎皮脚本作者: [player] 加入服务器！",
-        notification = "皮脚本作者 [player] 加入服务器"
+        notification = "皮脚本作者 [player] 加入服务器",
+        showOtherTitle = true,
+        receiveWelcomeMsg = false,
+        receiveNotification = true
     },
-        ["xiaopi91"] = {
-        title = "皮脚本VIP用户",
-        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
-        notification = "皮脚本VIP用户 [player] 加入服务器"
-    },
-    ["CYYNBNBN"] = {
+    ["dsafaac"] = {
         title = "长夜月",
-        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
-        notification = "温柔可爱长夜月已进入服务器"
+        welcomeMsg = "😍欢迎美丽的长夜月进入服务器",
+        notification = "温柔可爱长夜月已进入服务器",
+        showOtherTitle = false,        -- 不显示其他VIP头衔
+        receiveWelcomeMsg = true,      -- 接收欢迎消息
+        receiveNotification = false    -- 不接收通知
     }
 }
