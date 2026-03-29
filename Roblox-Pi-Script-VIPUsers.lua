1828eb222e5f0970966d77729b6756409fc43b63
xiaopi77 <2131869117@qq.com>
2026-03-29 10:14:32 +0800

    Refactor VIP user messages in Roblox script

--- Roblox-Pi-Script-VIPUsers.lua
+++ Roblox-Pi-Script-VIPUsers.lua
@@ -1,25 +1,17 @@
 return {
     ["xiaopi333"] = {
         title = "皮脚本作者",
-        chatMessage = "👑欢迎皮脚本作者: [player] 加入服务器！",
+        welcomeMsg = "👑欢迎皮脚本作者: [player] 加入服务器！",
         notification = "皮脚本作者: [player] 加入服务器"
     },
-    
     ["xiaopi91"] = {
         title = "皮脚本作者",
-        chatMessage = "👑欢迎皮脚本作者: [player] 加入服务器！",
+        welcomeMsg = "👑欢迎皮脚本作者: [player] 加入服务器！",
         notification = "皮脚本作者 [player] 加入服务器"
     },
-    
-    ["chinawerui"] = {
-        title = "叶脚本作者",
-        chatMessage = "👑欢迎叶脚本作者: [player] 加入服务器！",
-        notification = "叶脚本作者 [player] 加入服务器"
-    },
-    
     ["CYYNBNBN"] = {
         title = "长夜月",
-        chatMessage = "欢迎皮脚本VIP用户进入服务器",
-        notification = "皮脚本VIP用户 [player] 加入服务器"
-    },    
+        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
+        notification = "温柔可爱长夜月已进入服务器"
+    }
 }
