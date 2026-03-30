27f627b93931fb959d6abe439cfe3a4873206964
xiaopi77 <2131869117@qq.com>
2026-03-31 00:37:29 +0800

    Update Roblox-Pi-Script-VIPUsers.lua

--- Roblox-Pi-Script-VIPUsers.lua
+++ Roblox-Pi-Script-VIPUsers.lua
@@ -3,24 +3,88 @@ return {
         title = "皮脚本作者",
         welcomeMsg = "👑欢迎皮脚本作者: [player] 加入服务器！",
         notification = "皮脚本作者: [player] 加入服务器",
-        visibleToOtherVIPs = true,      -- 是否让其他VIP用户看到此用户的头衔
+        visibleToOtherVIPs = false,      -- 是否让其他VIP用户看到此用户的头衔
         receiveWelcomeMsgs = false,      -- 是否接收其他VIP用户的欢迎消息
-        receiveNotifications = true     -- 是否接收其他VIP用户的通知
+        receiveNotifications = false     -- 是否接收其他VIP用户的通知
     },
-    ["xiaopi91"] = {
-        title = "皮脚本作者",
-        welcomeMsg = "👑欢迎皮脚本作者: [player] 加入服务器！",
-        notification = "皮脚本作者 [player] 加入服务器",
+        ["78lwq23"] = {
+        title = "皮脚本作者女朋友",
+        welcomeMsg = "👑欢迎皮脚本作者女朋友: [player] 加入服务器！",
+        notification = "皮脚本作者女朋友: [player] 加入服务器",
+        visibleToOtherVIPs = false,      
+        receiveWelcomeMsgs = false,     
+        receiveNotifications = false     
+    },
+    ["top_cc515"] = {
+        title = "皮脚本VIP用户",
+        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
+        notification = "皮脚本VIP用户 [player] 加入服务器",
         visibleToOtherVIPs = true,
-        receiveWelcomeMsgs = true,
+        receiveWelcomeMsgs = false,
+        receiveNotifications = true
+    },
+      ["gfcghj089"] = {
+        title = "皮脚本VIP用户",
+        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
+        notification = "皮脚本VIP用户 [player] 加入服务器",
+        visibleToOtherVIPs = true,
+        receiveWelcomeMsgs = false,
+        receiveNotifications = true
+    },
+          ["EOVXO1"] = {
+        title = "皮脚本VIP用户",
+        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
+        notification = "皮脚本VIP用户 [player] 加入服务器",
+        visibleToOtherVIPs = true,
+        receiveWelcomeMsgs = false,
+        receiveNotifications = true
+    },
+     ["78vs91v"] = {
+        title = "皮脚本VIP用户",
+        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
+        notification = "皮脚本VIP用户 [player] 加入服务器",
+        visibleToOtherVIPs = true,
+        receiveWelcomeMsgs = false,
+        receiveNotifications = true
+    },
+         ["gao520l"] = {
+        title = "落叶HUB作者",
+        welcomeMsg = "欢迎落叶HUB作者: [player] 加入服务器！",
+        notification = "落叶HUB作者 [player] 加入服务器",
+        visibleToOtherVIPs = true,
+        receiveWelcomeMsgs = false,
+        receiveNotifications = true
+    },
+             ["Hudson_From4"] = {
+        title = "云脚本作者",
+        welcomeMsg = "欢迎云脚本作者: [player] 加入服务器！",
+        notification = "云脚本作者 [player] 加入服务器",
+        visibleToOtherVIPs = true,
+        receiveWelcomeMsgs = false,
+        receiveNotifications = true
+    },
+                 ["chinawerui"] = {
+        title = "叶脚本作者",
+        welcomeMsg = "欢迎叶脚本作者: [player] 加入服务器！",
+        notification = "叶脚本作者 [player] 加入服务器",
+        visibleToOtherVIPs = true,
+        receiveWelcomeMsgs = false,
+        receiveNotifications = true
+    },
+    ["CYYNBNBN"] = {
+        title = "皮脚本管理",
+        welcomeMsg = "欢迎皮脚本管理: [player] 加入服务器！",
+        notification = "皮脚本管理 [player] 加入服务器",
+        visibleToOtherVIPs = true,
+        receiveWelcomeMsgs = false,
         receiveNotifications = true
     },
     ["wzhxIl"] = {
-        title = "皮脚本VIP用户",
-        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
-        notification = "皮脚本VIP用户已进入服务器",
-        visibleToOtherVIPs = false,    
-        receiveWelcomeMsgs = true,      
+        title = "皮脚本作者徒弟",
+        welcomeMsg = "欢迎皮脚本作者徒弟: [player] 加入服务器！",
+        notification = "皮脚本作者徒弟已进入服务器",
+        visibleToOtherVIPs = true,    
+        receiveWelcomeMsgs = false,      
         receiveNotifications = true    
     }
 }
