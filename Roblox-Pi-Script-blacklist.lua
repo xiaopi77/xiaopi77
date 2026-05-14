16a3e7008aab8fc5d33e3259e55f6fde708bff63
xiaopi77 <2131869117@qq.com>
2026-05-14 13:33:55 +0800

    Update Roblox-Pi-Script-blacklist.lua

--- Roblox-Pi-Script-blacklist.lua
+++ Roblox-Pi-Script-blacklist.lua
@@ -1,6 +1,6 @@
 return {
-    -- 用户名
-    ["myt33996677"] = true,--司马小学生瞎甩
+    -- ========== 用户名（不带原因，显示默认） ==========
+    ["myt33996677"] = true,
     ["cnmby2"] = true,
     ["qwerasdfzx051"] = true,
     ["xi_di39"] = true,
@@ -8,17 +8,12 @@ return {
     ["owsksjsiwks"] = true,
     ["7891V54"] = true,
     ["mgdyaj"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    ["用户名"] = true,
-    -- HWID
+    
+    -- ========== 用户名（带原因，显示自定义） ==========
+    ["xiaopi333"] = "开挂行为",
+    ["示例用户B"] = "辱骂管理员",
+    
+    -- ========== HWID（不带原因，显示默认） ==========
     ["48de6142dd7bf15fc6685090ef4c9d2987fe9d7dc1031ea06b6c05a8525defda"] = true,
     ["d75339c67c712ae083afb5ce9838395f1052ba0a2e132f0260596abb2bf8acc2"] = true,
     ["f6a191f1ea57651f45c1d0fafdf5b922233af2392c07b5c64c07599de780a75b"] = true,
@@ -26,15 +21,8 @@ return {
     ["4d6aaad34c1eb5c5e917239eeca4393ebc5e3e10aaf2f323f17949f61c049f7c"] = true,
     ["84c7f5f27f5cb425ee479df23c132588f1897495e1703cf559de6a36aa1f5fc6"] = true,
     ["e7ad538dfade7c93698dc0751b8810714b78deda67f1e11a4bf6194ac593e09c"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
-    ["HWID"] = true,
+    
+    -- ========== HWID（带原因，显示自定义） ==========
+    ["示例HWID_A"] = "开挂被封",
+    ["示例HWID_B"] = "恶意行为",
 }
