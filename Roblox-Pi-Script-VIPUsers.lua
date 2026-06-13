3157d55b32ebf12eb276d9b0a8511699b7328f2c
xiaopi77 <2131869117@qq.com>
2026-06-14 03:53:29 +0800

    Update Roblox-Pi-Script-VIPUsers.lua

--- Roblox-Pi-Script-VIPUsers.lua
+++ Roblox-Pi-Script-VIPUsers.lua
@@ -71,6 +71,14 @@ return {
         receiveWelcomeMsgs = false,
         receiveNotifications = true
     },
+        ["idgduv4"] = {
+        title = "皮脚本VIP用户",
+        welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
+        notification = "皮脚本VIP用户 [player] 加入服务器",
+        visibleToOtherVIPs = true,
+        receiveWelcomeMsgs = false,
+        receiveNotifications = true
+    },
     ["ajs1145140"] = {
         title = "皮脚本VIP用户",
         welcomeMsg = "欢迎皮脚本VIP用户: [player] 加入服务器！",
